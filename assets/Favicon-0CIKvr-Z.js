7c0fc5f7bbfb077d8acd0f9ed9732c37c88db3ae
dipu-bd <dipu-bd@users.noreply.github.com>
2026-03-27 17:16:38 +0000

    deploy: 40a0106d7dae1c2f3d28c1ad8a000d9e3edec511

--- assets/Favicon-0CIKvr-Z.js
+++
@@ -1,1 +0,0 @@
-import{q as n,e as i,j as o}from"./index-Ccu6h24Z.js";import{a as e,A as c}from"./vendor-antd-CMvu2Lxp.js";import{x as m}from"./vendor-antd-icons-B6_7dtMt.js";const j=({url:s,...r})=>{const a=e.useMemo(()=>{if(!s)return null;const t=n.stringify({url:s});return`${i}/api/meta/favicon?${t}`},[s]);return o.jsx(c,{size:24,icon:o.jsx(m,{}),...r,src:a})};export{j as F};
